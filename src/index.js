c4e8c803cf8dd92bebe34d6ba5dc10e10704bb07
Geovani de Souza <geovanisouza92@gmail.com>
2017-09-07 18:08:40 -0300

    Styles setup done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from "lodash";
+import "./styles.css";
 
 function component() {
   var element = document.createElement("div");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const dist = path.resolve(__dirname, "dist");
 
@@ -9,7 +10,19 @@ module.exports = {
     path: dist,
     filename: "bundle.js"
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  },
   plugins: [
+    new ExtractTextPlugin("styles.css"),
     new HtmlWebpackPlugin({
       template: "index.html"
     })
